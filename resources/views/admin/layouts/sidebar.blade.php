d8819d795dbe6db5fce356b76c3d222a05bd4099
John <sandbox.duchuy02@gmail.com>
2024-12-19 10:49:37 +0700

    init project

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,27 @@
+<div class="sidebar-wrapper scrollbar scrollbar-inner">
+    <div class="sidebar-content">
+        <ul class="nav nav-secondary">
+            <li class="nav-item">
+                <a
+                    data-bs-toggle="collapse"
+                    href="#dashboard"
+                    class="collapsed"
+                    aria-expanded="false"
+                >
+                    <i class="fas fa-bars"></i>
+                    <p>Quản lý người dùng</p>
+                    <span class="caret"></span>
+                </a>
+                <div class="collapse" id="dashboard">
+                    <ul class="nav nav-collapse">
+                        <li class="">
+                            <a href="">
+                                <span class="sub-item">Danh sách người dùng</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
